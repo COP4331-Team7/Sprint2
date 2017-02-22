1f3628834116b003bbf8a0555143206f37e802cb
Or Oz <oroz77@gmail.com>
2017-02-22 16:18:27 -0500

    added Resource generation to Tile

--- src/com/team7/model/Tile.java
+++ src/com/team7/model/Tile.java
@@ -1,10 +1,14 @@
 package com.team7.model;
 
+import com.team7.ProbabilityGenerator;
 import com.team7.model.areaEffects.AreaEffect;
 import com.team7.model.decal.Decal;
 import com.team7.model.item.Item;
+import com.team7.model.resource.Energy;
+import com.team7.model.resource.Food;
+import com.team7.model.resource.Ore;
 import com.team7.model.resource.Resource;
-import com.team7.model.terrain.Terrain;
+import com.team7.model.terrain.*;
 
 /**
  * Hex shaped which builds the game map
@@ -24,5 +28,63 @@ public class Tile {
     //a Tile must have a terrain
     public Tile(Terrain terrain){
         this.terrain = terrain;
+
+        populateTileBasedOnTerrain(terrain);
+
+    }
+
+    private void populateTileBasedOnTerrain(Terrain terrain) {
+
+        //check tile terrain
+        //depending on terrain type:
+        //Desert has 10% AreaEffect, 5% Item, 5% Resource
+        //FlatLand has 20% AreaEffect, 15% Item, 30% Resource
+        //Crater has 20% AreaEffect, 5% Item, 25% Resource
+        //Mountain has 0% AreaEffect, 0% Item, 0% Resource
+
+        //AreaEffect, Item, and Resource generation depend on terrain type
+        if(terrain instanceof Desert){
+            //populateAreaEffect(0.1);
+           //populateItem(0.05);
+            populateResource(0.05);
+
+        }
+        else if(terrain instanceof Flatland){
+            //populateAreaEffects(0.2);
+          //populateItem(tile,0.15);
+            populateResource(0.3);
+        }
+        else if(terrain instanceof Crater){
+            //populateAreaEffects(0.2);
+            //populateItem(0.05);
+            populateResource(0.25);
+        }
+        else if(terrain instanceof Mountains){
+            //populateAreaEffects(0);
+            //populateItem(0);
+            populateResource(0);
+        }
+    }
+
+
+    // Populate Resource for each tile.
+    private void populateResource(double prob) {
+        if(ProbabilityGenerator.willOccur(prob)){
+            int rand = ProbabilityGenerator.randomInteger(0,2);
+            if(rand == 0)
+                setResource(new Energy());
+            else if(rand == 1)
+                setResource(new Food());
+            else if(rand == 2)
+                setResource(new Ore());
+        }
+    }
+
+    public Resource getResource() {
+        return resource;
+    }
+
+    public void setResource(Resource resource) {
+        this.resource = resource;
     }
 }
