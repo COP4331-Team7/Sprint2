246e12ac759ab52d8acf7c5b4271c97f2f38706b
Or Oz <oroz77@gmail.com>
2017-02-23 09:10:34 -0500

    added Resource as Visitable

--- src/com/team7/model/resource/Resource.java
+++ src/com/team7/model/resource/Resource.java
@@ -1,11 +1,14 @@
 package com.team7.model.resource;
 
+import com.team7.model.visitor.Visitable;
+import com.team7.model.visitor.Visitor;
+
 /**
  * Superclass of a Resource
  * May be on a Tile in a discrete quantity
  * Harvestable
  */
-public abstract class Resource {
+public abstract class Resource implements Visitable {
     private boolean isDiscovered; //a Resource isDiscovered when it is revealed by Explorer
     private int statInfluenceQuantity; //affects Power, Nutrients, or Metal
     abstract void addResourceToPlayer(int harvestedQuantity);
@@ -24,5 +27,11 @@ public abstract class Resource {
 
     public void setStatInfluenceQuantity(int statInfluenceQuantity) {
         this.statInfluenceQuantity = statInfluenceQuantity;
+    }
+
+    //a Visitable resource will tell the Player how much to increase each stat
+    @Override
+    public void accept(Visitor visitor) {
+        visitor.visit(this);
     }
 }
