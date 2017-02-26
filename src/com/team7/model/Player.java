00112679bed314f0981d9f4a659c4d997ea50c22
acoverstone <acoverstone@ufl.edu>
2017-02-26 11:00:40 -0500

    Added basic player and game

--- src/com/team7/model/Player.java
+++ src/com/team7/model/Player.java
@@ -1,5 +1,52 @@
 package com.team7.model;
 
+import com.team7.model.entity.Army;
+import com.team7.model.entity.structure.Structure;
+import com.team7.model.entity.unit.Unit;
+
+import java.util.ArrayList;
+
 public class Player {
-    public Player() {}
+    private ArrayList<Unit> units;
+    private ArrayList<Structure> structures;
+    private ArrayList<Army> armies;
+    private int research;
+    private int construction;
+    private int money;
+    private boolean noUnits;
+    private boolean noStructures;
+    private boolean noArmies;
+
+    public Player() {
+        units = new ArrayList<Unit>();                               // max size should be 25
+        structures = new ArrayList<Structure>();                     // max size should be 10
+        armies = new ArrayList<Army>();                              // max size should be 10
+        research = 0;
+        construction = 0;
+        money = 500;
+        noUnits = true;
+        noStructures = true;
+        noArmies = true;
+    }
+
+
+
+
+
+    public boolean isDefeated() {
+        return noArmies && noUnits && noStructures;
+    }
+
+    public boolean isNoUnits() {
+        return noUnits;
+    }
+
+    public boolean isNoStructures() {
+        return noStructures;
+    }
+
+    public boolean isNoArmies() {
+        return noArmies;
+    }
+
 }
