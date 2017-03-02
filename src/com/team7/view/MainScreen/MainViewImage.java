87955412e653695e114ac50c3cbd652303e508fc
thomas1242 <thomaspalmer121@gmail.com>
2017-03-02 00:44:21 -0500

    map

--- src/com/team7/view/MainScreen/MainViewImage.java
+++ src/com/team7/view/MainScreen/MainViewImage.java
@@ -18,46 +18,36 @@ import java.io.IOException;
 
 public class MainViewImage extends JPanel implements MouseListener, MapStats {
 
-        public static BufferedImage image;
-        private Graphics2D g2d;
         private int MAP_IMAGE_WIDTH_IN_PIXELS;
         private int MAP_IMAGE_HEIGHT_IN_PIXELS;
-
-        private int TILES_VISIBLE_X;
+        private int TILES_VISIBLE_X;                    // # tiles visible on map
         private int TILES_VISIBLE_Y;
-
-        private final static double mapScale_x = 0.95;
+        private final static double mapScale_x = 0.95;  // what % of screen the map takes up
         private final static double mapScale_y = 0.60;
+
+        private Graphics2D g2d, g2ds;
+        public static BufferedImage image, tempImg;
+        private BufferedImage tileImage_1, tileImage_2, tileImage_3, tileImage_4;
+        private BufferedImage moneyBagImage;
+        private BufferedImage moonRockImage;
+        private BufferedImage hieroglyphicBookImage;
+        private BufferedImage elixerShowerImage;
+        private BufferedImage stormImage;
+        private BufferedImage colonistImage;
+        private BufferedImage explorerImage;
+        private BufferedImage obstacleImage;
+        private BufferedImage oneShotImage;
+        private BufferedImage meleeImage;
+        private BufferedImage rangeImage;
+        private BufferedImage armyImage;
+        private BufferedImage ventImage;
+        private BufferedImage skullImage;
+        private BufferedImage baseImage;
 
         public int x_center, y_center;    // where the window is focused on
         public int x_dest, y_dest;        // where the window should be focused on
 
-        private BufferedImage tileImage_1, tileImage_2, tileImage_3, tileImage_4;
-
-        private BufferedImage moneyBagImage;
-        private BufferedImage moonRockImage;
-        private BufferedImage hieroglyphicBookImage;
-
-        private BufferedImage elixerShowerImage;
-        private BufferedImage stormImage;
-
-        private  BufferedImage colonistImage;
-        private  BufferedImage explorerImage;
-
-        private  BufferedImage obstacleImage;
-        private  BufferedImage oneShotImage;
-
-        private  BufferedImage meleeImage;
-        private  BufferedImage rangeImage;
-        private  BufferedImage armyImage;
-        private  BufferedImage ventImage;
-
-        private  BufferedImage skullImage;
-        private  BufferedImage baseImage;
-        BufferedImage tempImg ;
-        Graphics2D g2ds;
         private MainViewMiniMap mainViewSelection;
-
         private Tile[][] grid;
 
         public MainViewImage( MainViewMiniMap ms )
@@ -79,40 +69,27 @@ public class MainViewImage extends JPanel implements MouseListener, MapStats {
                tileImage_2 = ImageIO.read(Main.class.getClass().getResourceAsStream("/terrains/a.png"));
                tileImage_3 = ImageIO.read(Main.class.getClass().getResourceAsStream("/terrains/c.png"));
                tileImage_4 = ImageIO.read(Main.class.getClass().getResourceAsStream("/terrains/c.png"));
-
                moneyBagImage = ImageIO.read(Main.class.getClass().getResourceAsStream("/items/moneyBag.png"));
                moonRockImage = ImageIO.read(Main.class.getClass().getResourceAsStream("/items/moonRock.png"));
                hieroglyphicBookImage = ImageIO.read(Main.class.getClass().getResourceAsStream("/items/bookImage1.png"));
-
                elixerShowerImage = ImageIO.read(Main.class.getClass().getResourceAsStream("/areaEffects/elixirShowerImage.png"));
                stormImage = ImageIO.read(Main.class.getClass().getResourceAsStream("/areaEffects/stormImageBig.png"));
                colonistImage = ImageIO.read(Main.class.getClass().getResourceAsStream("/units/colonistImage.png"));
                explorerImage = ImageIO.read(Main.class.getClass().getResourceAsStream("/units/explorerImage.png"));
-
                oneShotImage = ImageIO.read(Main.class.getClass().getResourceAsStream("/obstacles/oneShot.png"));
                obstacleImage = ImageIO.read(Main.class.getClass().getResourceAsStream("/obstacles/stopIcon.png"));
-
-                meleeImage = ImageIO.read(Main.class.getClass().getResourceAsStream("/units/meleeImage.png"));
-                rangeImage = ImageIO.read(Main.class.getClass().getResourceAsStream("/units/rangeImage.png"));
-                armyImage = ImageIO.read(Main.class.getClass().getResourceAsStream("/units/armyImagepng.png"));
-                ventImage = ImageIO.read(Main.class.getClass().getResourceAsStream("/areaEffects/vent.png"));
-
-                skullImage = ImageIO.read(Main.class.getClass().getResourceAsStream("/decals/skullImage.png"));
-
-                baseImage = ImageIO.read(Main.class.getClass().getResourceAsStream("/structures/baseImage.png"));
-
+               meleeImage = ImageIO.read(Main.class.getClass().getResourceAsStream("/units/meleeImage.png"));
+               rangeImage = ImageIO.read(Main.class.getClass().getResourceAsStream("/units/rangeImage.png"));
+               armyImage = ImageIO.read(Main.class.getClass().getResourceAsStream("/units/armyImagepng.png"));
+               ventImage = ImageIO.read(Main.class.getClass().getResourceAsStream("/areaEffects/vent.png"));
+               skullImage = ImageIO.read(Main.class.getClass().getResourceAsStream("/decals/skullImage.png"));
+               baseImage = ImageIO.read(Main.class.getClass().getResourceAsStream("/structures/baseImage.png"));
             }
             catch (IOException e) {}
 
             mainViewSelection.setMainViewImage( this );
-
             tempImg = new BufferedImage( MAP_IMAGE_WIDTH_IN_PIXELS, MAP_IMAGE_HEIGHT_IN_PIXELS, BufferedImage.TYPE_INT_ARGB);
             g2ds = (Graphics2D)tempImg.createGraphics();
-
-            x_center = 0;   // initially focus on top left corner of the map
-            y_center = 0;
-            x_dest = 0;
-            y_dest = 0;
         }
 
         public void setMap(Map map) {
@@ -120,25 +97,13 @@ public class MainViewImage extends JPanel implements MouseListener, MapStats {
             mainViewSelection.setMiniMapImage( getFullMapImage(), TILES_VISIBLE_X, TILES_VISIBLE_Y );
         }
 
-//        public void setCurrentPlayer( Player player ) {
-//            this.currentPlayer = player;
-//            // focus on one of the current players units.
-//            ArrayList<Unit> units = (ArrayList<Unit>) currentPlayer.getUnits();
-//            if( !units.isEmpty() ) {
-//                zoomToDestination( units.get(0).getLocation().getxCoordinate() - TILES_VISIBLE_X/2, units.get(0).getLocation().getyCoordinate() - TILES_VISIBLE_Y/2, 50);
-//            }
-//         }
-
          public BufferedImage getFullMapImage() {
              BufferedImage mapImage;
-             int tempInt = (int)(MAP_TILE_WIDTH * (TILE_SIZE - TILE_SIZE / 1.73) + TILE_SIZE);
-             mapImage = new BufferedImage(TILE_SIZE*MAP_TILE_WIDTH + tempInt  , (int)(TILE_SIZE*MAP_TILE_HEIGHT/1.5), BufferedImage.TYPE_INT_ARGB);
+             mapImage = new BufferedImage(TILE_SIZE*MAP_TILE_WIDTH + (int)(MAP_TILE_WIDTH * (TILE_SIZE - TILE_SIZE / 1.73) + TILE_SIZE)  , (int)(TILE_SIZE*MAP_TILE_HEIGHT/1.5), BufferedImage.TYPE_INT_ARGB);
              Graphics2D g2 = (Graphics2D)mapImage.createGraphics();
              int x_coord, y_coord;   // pixel coordinates of top left corner of image drawn
-             int x_offset = 0;
-             int counter = 0;
+             int x_offset, counter, step = 0;
              int changePerStep = TILE_SIZE - (int)(TILE_SIZE/1.73);
-             int step = 0;
 
              for(int j = 0; j < MAP_TILE_WIDTH; j++) {          // tile index on sub-screen
                  x_offset = changePerStep;
@@ -150,18 +115,17 @@ public class MainViewImage extends JPanel implements MouseListener, MapStats {
                      x_offset += 10;
                  }
                  step++;
-                 for(int i = 0; i < MAP_TILE_HEIGHT; i++) {
 
+                 for(int i = 0; i < MAP_TILE_HEIGHT; i++) {
                      counter = 1;
 
-                     int xx = i;                // tile index on whole map
+                     int xx = i;                   //  index in Map's grid[][]
                      int yy = j;
 
                      if(xx < 0)                     // adjust if out of bounds
                          xx = 0;
                      else if (xx >= MAP_TILE_WIDTH)
                          xx = MAP_TILE_WIDTH - 1;
-
                      if(yy < 0)
                          yy = 0;
                      else if(yy >= MAP_TILE_HEIGHT)
@@ -170,8 +134,6 @@ public class MainViewImage extends JPanel implements MouseListener, MapStats {
                      x_coord = i * TILE_SIZE;
                      y_coord = (int)(j * TILE_SIZE / 2.4);
 
-
-                     // draw terrain
                      if( grid[xx][yy].getTerrain() instanceof Mountains) {
                          g2.drawImage(tileImage_1, x_coord + x_offset, y_coord, null);
                          x_offset += changePerStep * counter;
@@ -192,24 +154,20 @@ public class MainViewImage extends JPanel implements MouseListener, MapStats {
                          x_offset += changePerStep * counter;
                          counter++;
                      }
-
                  }
              }
 
              return mapImage;
-
          }
 
         private BufferedImage drawSubsectionOfMap(int x, int y) {
-
             g2ds.setFont(new Font("default", Font.BOLD, 11));
             g2ds.setColor( new Color(230, 230, 230, 140)  );
 
             int x_coord, y_coord;   // pixel coordinates of top left corner of image drawn
-            int x_offset = 0;
-            int counter = 0;
+            int x_offset, counter, step = 0;
             int changePerStep = TILE_SIZE - (int)(TILE_SIZE/1.73);
-            int step = 0;
+
             for(int j = 0; j < TILES_VISIBLE_X; j++) {          // tile index on sub-screen
                 x_offset = changePerStep;
                 if(step % 2 == 0) {
@@ -220,8 +178,8 @@ public class MainViewImage extends JPanel implements MouseListener, MapStats {
                     x_offset += 10;
                 }
                 step++;
-                for(int i = 0; i < TILES_VISIBLE_Y; i++) {
 
+                for(int i = 0; i < TILES_VISIBLE_Y; i++) {
                 counter = 1;
 
                     int xx = x + i;                // tile index on whole map
@@ -231,7 +189,6 @@ public class MainViewImage extends JPanel implements MouseListener, MapStats {
                         xx = 0;
                     else if (xx >= MAP_TILE_WIDTH)
                         xx = MAP_TILE_WIDTH - 1;
-
                     if(yy < 0)
                         yy = 0;
                     else if(yy >= MAP_TILE_HEIGHT)
@@ -240,7 +197,6 @@ public class MainViewImage extends JPanel implements MouseListener, MapStats {
                     x_coord = i * TILE_SIZE;
                     y_coord = (int)(j * TILE_SIZE / 2.4);
 
-                    // draw terrain
                     if( grid[xx][yy].getTerrain() instanceof Mountains) {
                         g2ds.drawImage(tileImage_1, x_coord + x_offset, y_coord, null);
                         x_offset += changePerStep * counter;
@@ -262,29 +218,20 @@ public class MainViewImage extends JPanel implements MouseListener, MapStats {
                         counter++;
                     }
 
-                    int s1_x = -15;
-                    int s1_y =  31;
+                    int s1_x = -15, s1_y =  31;
 
                     g2ds.setColor( new Color(0, 30, 230, 90)  ); // blue
-
                     if(drawOnTile) {
-
                         g2ds.fillOval(x_coord + x_offset + s1_x, y_coord + s1_y, 18, 18);
-
                         g2ds.setColor(new Color(255, 255, 100, 70));
                         g2ds.setColor(new Color(255, 128, 100, 150));
-
                         g2ds.fillOval(x_coord + x_offset + 5, y_coord + s1_y, 18, 18);
-
                         g2ds.setColor(new Color(0, 0, 0, 255));
-
                         String s = Integer.toString(xx);
                         g2ds.drawString(s, x_coord + x_offset + s1_x + 3, y_coord + s1_y + 13);
-
                         s = Integer.toString(yy);
                         g2ds.drawString(s, x_coord + x_offset + s1_x + 22, y_coord + s1_y + 13);
                     }
-
                 }
             }
 
@@ -311,21 +258,16 @@ public class MainViewImage extends JPanel implements MouseListener, MapStats {
             return image;
         }
 
-        // implement MouseListener interface methods:
         public void mousePressed(MouseEvent e) {}
-
         public void mouseReleased(MouseEvent e) {}
-
         public void mouseEntered(MouseEvent e) {}
-
         public void mouseExited(MouseEvent e) {}
-
         public void mouseClicked(MouseEvent e) {
 
             int center_pixel_x = MAP_IMAGE_WIDTH_IN_PIXELS  / 2;
             int center_pixel_y = MAP_IMAGE_HEIGHT_IN_PIXELS / 2;
-           double x_offset = (e.getX() - center_pixel_x)/(double)TILE_SIZE;   // offset in number of tiles
-           double y_offset = -1*(center_pixel_y - e.getY())/(double)TILE_SIZE;
+            double x_offset = (e.getX() - center_pixel_x)/(double)TILE_SIZE;   // offset in number of tiles
+            double y_offset = -1*(center_pixel_y - e.getY())/(double)TILE_SIZE;
 
            if(x_offset % 1 > 0.5) {
             x_offset += 1;
@@ -333,7 +275,6 @@ public class MainViewImage extends JPanel implements MouseListener, MapStats {
            else if(x_offset % 1 < -0.5) {
             x_offset += -1;
            }
-
            if(y_offset % 1 > 0.5) {
             y_offset += 1;
            }
@@ -344,11 +285,9 @@ public class MainViewImage extends JPanel implements MouseListener, MapStats {
             //System.out.println("offset (" + (int)x_offset + ", " + (int)y_offset + ")" );
             x_dest = x_center + (int)x_offset;
             y_dest = y_center + (int)(y_offset * 1.5);
-
            //System.out.println("center (" + (int)x_center + ", " + (int)y_center + ")" );
            // System.out.println("dest (" + (int)x_dest + ", " + (int)y_dest + ")" );
-
-            zoomToDestination( x_dest, y_dest, 100 );
+            zoomToDestination( x_dest, y_dest, 50 );
         }
 
         public void zoomToDestination(int x_dest, int y_dest, int delayInMs) {
@@ -357,7 +296,6 @@ public class MainViewImage extends JPanel implements MouseListener, MapStats {
                 x_dest = 0;
             else if (x_dest >= MAP_TILE_WIDTH - TILES_VISIBLE_Y)
                 x_dest = MAP_TILE_WIDTH - TILES_VISIBLE_Y;
-
             if(y_dest < 0)
                 y_dest = 0;
             else if(y_dest >= MAP_TILE_HEIGHT - TILES_VISIBLE_X)
@@ -365,11 +303,8 @@ public class MainViewImage extends JPanel implements MouseListener, MapStats {
 
             final int x_destination  = x_dest;
             final int y_destination  = y_dest;
-
             //System.out.println("zoom to: " + x_dest + "," + y_dest);
-
             if( x_center != x_destination || y_center != y_destination) {
-
                 new Thread( new Runnable()
                 {
                     public void run()
@@ -387,7 +322,6 @@ public class MainViewImage extends JPanel implements MouseListener, MapStats {
                         }
 
                         while (x_diff != 0 || y_diff != 0) {    // while view isnt focused on destination tile
-
                             if(x_diff != 0) {                   // move focus 1 unit towards destination
                                 x_center += delta_x;
                                 x_diff -= delta_x;
@@ -398,7 +332,6 @@ public class MainViewImage extends JPanel implements MouseListener, MapStats {
                             }
 
                            // System.out.println("get frame focus at (" + (int)x_center + ", " + (int)y_center + ")" );
-
                             final BufferedImage mapSubsection = drawSubsectionOfMap(x_center, y_center);
                             mainViewSelection.setFocus(x_center, y_center);
 
@@ -415,11 +348,18 @@ public class MainViewImage extends JPanel implements MouseListener, MapStats {
                                 Thread.sleep(delayInMs);
                             }
                             catch(Exception e) {}
-
                         }   // end of while loop
                     }
                 } ).start();
             }
         }
 
+    //        public void setCurrentPlayer( Player player ) {
+//            this.currentPlayer = player;
+//            // focus on one of the current players units.
+//            ArrayList<Unit> units = (ArrayList<Unit>) currentPlayer.getUnits();
+//            if( !units.isEmpty() ) {
+//                zoomToDestination( units.get(0).getLocation().getxCoordinate() - TILES_VISIBLE_X/2, units.get(0).getLocation().getyCoordinate() - TILES_VISIBLE_Y/2, 50);
+//            }
+//         }
 }
