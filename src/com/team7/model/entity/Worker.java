58ade21500dd5c28180d20c0901bdd7edee07a30
Or Oz <oroz77@gmail.com>
2017-03-05 11:34:10 -0500

    added more functionality to worker by adding more attributes

--- src/com/team7/model/entity/Worker.java
+++ src/com/team7/model/entity/Worker.java
@@ -7,6 +7,7 @@ public class Worker extends Entity {
 
     private Army army;
     private int constructionRate;
+    private int taskCompletionRate; //used for harvesting, technology advancements, training recruits
 
     public Worker(Tile startTile, Player player) {
         setOwner(player);
@@ -15,10 +16,23 @@ public class Worker extends Entity {
         setArmy(null);
         setVisibilityRadius(3);
         constructionRate = 10;
+        taskCompletionRate = 5;
     }
 
     public int getConstructionRate() {
         return constructionRate;
+    }
+
+    public int getTaskCompletionRate() {
+        return taskCompletionRate;
+    }
+
+    public void changeConstructionRate(int delta){
+        constructionRate += delta;
+    }
+
+    public void changeTaskCompletionRate(int delta){
+        taskCompletionRate += delta;
     }
 
     public Army getArmy() {
